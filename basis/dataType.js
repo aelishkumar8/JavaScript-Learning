0e33cb6eaea3cb82d35a42541d36361384bd09a2
aelishkumar8 <aelishkumar8@gmail.com>
2025-12-25 20:05:43 +0530

    Adding DataType

---
+++ basis/dataType.js
@@ -0,0 +1,18 @@
+//tc39.es -> website for standardized JavaScript features
+"use strict"; // Enforce strict mode
+let name = "Aelish"
+let age = 25
+let isStudent = true
+
+console.log(typeof age);      // Output: string
+
+/*
+Data Types in JavaScript:
+- Number => 2 to power 53 
+- Bigint => larger than 2 to power 53
+- String => ""
+- boolean => true/false
+- null => standalone value which is an object
+- undefined => it is undefined type 
+- symbol => unique identifier
+*/
--- basis/test.js
+++ basis/test.js
@@ -1,2 +1,22 @@
 // How to print "Hello, World!" to the console in JavaScript by node.js 
 console.log("Hello, World!");
+
+const accountId = 144434
+let accountEmail = "abe@gmail.com"
+var accountPassword = "12345"
+accountCity = "New York"
+let accountState; // undefined
+
+// accountId = 4        // not allowed, because accountId is a constant
+console.log(accountId);
+accountEmail = "hs"
+accountPassword = "asdasda"
+accountCity = "Los Angeles"
+
+/*
+    perfer to not use var
+    because it creates the variable in the global scope
+    even if it is defined inside a function or block 
+*/
+
+console.table([accountEmail, accountPassword, accountCity,accountState]);
